91e60f42c31cf5c9300d8b49d1fd76c5b826e15a
FernandoGomezP <fernandogomez@artelnics.com>
2015-12-21 12:52:55 +0100

    Deleted html

--- html/search/classes_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['histogram',['Histogram',['../struct_open_n_n_1_1_histogram.html',1,'OpenNN']]]
-];
--- html/search/enums_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['conditionsmethod',['ConditionsMethod',['../class_open_n_n_1_1_conditions_layer.html#ad88863a8742f729c3136062e916f1d6b',1,'OpenNN::ConditionsLayer']]],
-  ['constraintstype',['ConstraintsType',['../class_open_n_n_1_1_performance_functional.html#a3cb05a66b3525ab1a89a5472551447fe',1,'OpenNN::PerformanceFunctional']]],
-  ['crossovermethod',['CrossoverMethod',['../class_open_n_n_1_1_genetic_algorithm.html#ab581d2b4b313fa0f4063af8e8895d1eb',1,'OpenNN::GeneticAlgorithm']]]
-];
