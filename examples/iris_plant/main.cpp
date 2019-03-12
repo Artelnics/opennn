48e910281c03431cadfccaf7ca252331a9791a58
JavierSanchezB <javiersanchez@artelnics.com>
2019-03-12 13:52:28 +0100

    Fix examples source files.

--- examples/iris_plant/main.cpp
+++ examples/iris_plant/main.cpp
@@ -39,7 +39,7 @@ int main(void)
 
         DataSet data_set;
 
-        data_set.set_data_file_name("../data/iris_plant.dat");
+        data_set.set_data_file_name("./data/iris_plant.dat");
 
         data_set.set_separator("Space");
 
@@ -150,17 +150,17 @@ int main(void)
 
         // Save results
 
-        data_set.save("../data/data_set.xml");
+        data_set.save("./data/data_set.xml");
 
-        neural_network.save("../data/neural_network.xml");
-        neural_network.save_expression("../data/expression.txt");
+        neural_network.save("./data/neural_network.xml");
+        neural_network.save_expression("./data/expression.txt");
 
-        training_strategy.save("../data/training_strategy.xml");
+        training_strategy.save("./data/training_strategy.xml");
 
-//        model_selection.save("../data/model_selection.xml");
-//        model_selection_results.save("../data/model_selection_results.dat");
+//        model_selection.save("./data/model_selection.xml");
+//        model_selection_results.save("./data/model_selection_results.dat");
 
-        confusion.save("../data/confusion.dat");
+        confusion.save("./data/confusion.dat");
 
         return(0);
     }
--- examples/simple_pattern_recognition/main.cpp
+++ examples/simple_pattern_recognition/main.cpp
@@ -35,7 +35,7 @@ int main(void)
 
         DataSet data_set;
 
-        data_set.set_data_file_name("../data/simple_pattern_recognition.dat");
+        data_set.set_data_file_name("./data/simple_pattern_recognition.dat");
 
         data_set.load_data();
 
@@ -100,16 +100,16 @@ int main(void)
 
         // Save results
 
-        data_set.save("../data/data_set.xml");
+        data_set.save("./data/data_set.xml");
 
-        neural_network.save("../data/neural_network.xml");
-        neural_network.save_expression("../data/expression.txt");
+        neural_network.save("./data/neural_network.xml");
+        neural_network.save_expression("./data/expression.txt");
 
-        training_strategy.save("../data/training_strategy.xml");
-        training_strategy_results.save("../data/training_strategy_results.dat");
+        training_strategy.save("./data/training_strategy.xml");
+        training_strategy_results.save("./data/training_strategy_results.dat");
 
-        binary_classification_tests.save("../data/binary_classification_tests.dat");
-        confusion.save("../data/confusion.dat");
+        binary_classification_tests.save("./data/binary_classification_tests.dat");
+        confusion.save("./data/confusion.dat");
 
         return(0);
     }
