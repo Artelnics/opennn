3abd57ce135fafb65130c8879c9c8184bccd0c02
Roberto Lopez <robertolopez@artelnics.com>
2020-01-20 20:08:15 +0100

    Small changes

--- opennn/config.h
+++ opennn/config.h
@@ -2,27 +2,5 @@ typedef float type;
 
 #include "omp.h"
 
-/*
-class Device
-{
-    private:
-        // Private Constructor
-        Device();
 
-        // Stop the compiler generating methods of copy the object
 
-        Device(Device const& copy);            // Not Implemented
-
-        Device& operator=(Device const& copy); // Not Implemented
-
-    public:
-        static Device& getInstance()
-        {
-            // The only instance
-            // Guaranteed to be lazy initialized
-            // Guaranteed that it will be destroyed correctly
-            static Device instance;
-            return instance;
-        }
-};
-*/
