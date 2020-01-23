6ab95109ef9b4678e1715f9caa4b5eb35c37a8aa
Guillermo Gestoso <guillermogestoso@artelnics.com>
2020-01-23 19:26:23 +0100

    adam

--- opennn/adaptive_moment_estimation.cpp
+++ opennn/adaptive_moment_estimation.cpp
@@ -59,103 +59,103 @@ AdaptiveMomentEstimation::~AdaptiveMomentEstimation()
 
 /// Returns the initial learning rate.
 
-const double& AdaptiveMomentEstimation::get_initial_learning_rate() const
+const type& AdaptiveMomentEstimation::get_initial_learning_rate() const
 {
-   return(initial_learning_rate);
+   return initial_learning_rate;
 }
 
 
 /// Returns beta 1.
 
-const double& AdaptiveMomentEstimation::get_beta_1() const
+const type& AdaptiveMomentEstimation::get_beta_1() const
 {
-   return(beta_1);
+   return beta_1;
 }
 
 
 /// Returns beta 2.
 
-const double& AdaptiveMomentEstimation::get_beta_2() const
+const type& AdaptiveMomentEstimation::get_beta_2() const
 {
-   return(beta_2);
+   return beta_2;
 }
 
 
 /// Returns epsilon.
 
-const double& AdaptiveMomentEstimation::get_epsilon() const
+const type& AdaptiveMomentEstimation::get_epsilon() const
 {
-   return(epsilon);
+   return epsilon;
 }
 
 
 /// Returns the minimum value for the norm of the parameters vector at wich a warning message is
 /// written to the screen.
 
-const double& AdaptiveMomentEstimation::get_warning_parameters_norm() const
+const type& AdaptiveMomentEstimation::get_warning_parameters_norm() const
 {
-   return(warning_parameters_norm);
+   return warning_parameters_norm;
 }
 
 
 /// Returns the minimum value for the norm of the gradient vector at wich a warning message is written
 /// to the screen.
 
-const double& AdaptiveMomentEstimation::get_warning_gradient_norm() const
+const type& AdaptiveMomentEstimation::get_warning_gradient_norm() const
 {
-   return(warning_gradient_norm);
+   return warning_gradient_norm;
 }
 
 
 /// Returns the value for the norm of the parameters vector at wich an error message is
 /// written to the screen and the program exits.
 
-const double& AdaptiveMomentEstimation::get_error_parameters_norm() const
+const type& AdaptiveMomentEstimation::get_error_parameters_norm() const
 {
-   return(error_parameters_norm);
+   return error_parameters_norm;
 }
 
 
 /// Returns the value for the norm of the gradient vector at wich an error message is written
 /// to the screen and the program exits.
 
-const double& AdaptiveMomentEstimation::get_error_gradient_norm() const
+const type& AdaptiveMomentEstimation::get_error_gradient_norm() const
 {
-   return(error_gradient_norm);
+   return error_gradient_norm;
 }
 
 
 /// Returns the minimum norm of the parameter increment vector used as a stopping criteria when training.
 
-const double& AdaptiveMomentEstimation::get_minimum_parameters_increment_norm() const
+const type& AdaptiveMomentEstimation::get_minimum_parameters_increment_norm() const
 {
-   return(minimum_parameters_increment_norm);
+   return minimum_parameters_increment_norm;
 }
 
 
 /// Returns the minimum loss improvement during training.
 
-const double& AdaptiveMomentEstimation::get_minimum_loss_increase() const
+const type& AdaptiveMomentEstimation::get_minimum_loss_increase() const
 {
-   return(minimum_loss_decrease);
+   return minimum_loss_decrease;
 }
 
 
 /// Returns the goal value for the loss.
 /// This is used as a stopping criterion when training a neural network
 
-const double& AdaptiveMomentEstimation::get_loss_goal() const
+const type& AdaptiveMomentEstimation::get_loss_goal() const
 {
-   return(loss_goal);
+   return loss_goal;
 }
 
 
 /// Returns the goal value for the norm of the error function gradient.
 /// This is used as a stopping criterion when training a neural network
 
-const double& AdaptiveMomentEstimation::get_gradient_norm_goal() const
+const type& AdaptiveMomentEstimation::get_gradient_norm_goal() const
 {
-   return(gradient_norm_goal);
+   return gradient_norm_goal;
 }
 
 
@@ -163,15 +163,15 @@ const double& AdaptiveMomentEstimation::get_gradient_norm_goal() const
 
 const int& AdaptiveMomentEstimation::get_maximum_selection_failures() const
 {
-   return(maximum_selection_failures);
+   return maximum_selection_failures;
 }
 
 
 /// Returns the maximum training time.
 
-const double& AdaptiveMomentEstimation::get_maximum_time() const
+const type& AdaptiveMomentEstimation::get_maximum_time() const
 {
-   return(maximum_time);
+   return maximum_time;
 }
 
 
@@ -179,7 +179,7 @@ const double& AdaptiveMomentEstimation::get_maximum_time() const
 
 const bool& AdaptiveMomentEstimation::get_return_minimum_selection_error_neural_network() const
 {
-    return(return_minimum_selection_error_neural_network);
+    return return_minimum_selection_error_neural_network;
 }
 
 
@@ -187,7 +187,7 @@ const bool& AdaptiveMomentEstimation::get_return_minimum_selection_error_neural_
 
 const bool& AdaptiveMomentEstimation::get_apply_early_stopping() const
 {
-    return(apply_early_stopping);
+    return apply_early_stopping;
 }
 
 
@@ -195,7 +195,7 @@ const bool& AdaptiveMomentEstimation::get_apply_early_stopping() const
 
 const bool& AdaptiveMomentEstimation::get_reserve_training_error_history() const
 {
-   return(reserve_training_error_history);
+   return reserve_training_error_history;
 }
 
 
@@ -203,7 +203,7 @@ const bool& AdaptiveMomentEstimation::get_reserve_training_error_history() const
 
 const bool& AdaptiveMomentEstimation::get_reserve_selection_error_history() const
 {
-   return(reserve_selection_error_history);
+   return reserve_selection_error_history;
 }
 
 
@@ -284,7 +284,7 @@ void AdaptiveMomentEstimation::set_reserve_all_training_history(const bool& new_
 /// Sets a new learning rate.
 /// @param new_learning_rate.
 
-void AdaptiveMomentEstimation::set_initial_learning_rate(const double& new_learning_rate)
+void AdaptiveMomentEstimation::set_initial_learning_rate(const type& new_learning_rate)
 {
     initial_learning_rate= new_learning_rate;
 }
@@ -293,7 +293,7 @@ void AdaptiveMomentEstimation::set_initial_learning_rate(const double& new_learn
 /// Sets beta 1 generally close to 1.
 /// @param new_beta_1.
 
-void AdaptiveMomentEstimation::set_beta_1(const double& new_beta_1)
+void AdaptiveMomentEstimation::set_beta_1(const type& new_beta_1)
 {
     beta_1= new_beta_1;
 }
@@ -302,7 +302,7 @@ void AdaptiveMomentEstimation::set_beta_1(const double& new_beta_1)
 /// Sets beta 2 generally close to 1.
 /// @param new_beta_2.
 
-void AdaptiveMomentEstimation::set_beta_2(const double& new_beta_2)
+void AdaptiveMomentEstimation::set_beta_2(const type& new_beta_2)
 {
     beta_2= new_beta_2;
 }
@@ -311,7 +311,7 @@ void AdaptiveMomentEstimation::set_beta_2(const double& new_beta_2)
 /// Sets epsilon.
 /// @param epsilon.
 
-void AdaptiveMomentEstimation::set_epsilon(const double& new_epsilon)
+void AdaptiveMomentEstimation::set_epsilon(const type& new_epsilon)
 {
     epsilon= new_epsilon;
 }
@@ -320,10 +320,8 @@ void AdaptiveMomentEstimation::set_epsilon(const double& new_epsilon)
 /// Sets a new value for the parameters vector norm at which a warning message is written to the screen.
 /// @param new_warning_parameters_norm Warning norm of parameters vector value.
 
-void AdaptiveMomentEstimation::set_warning_parameters_norm(const double& new_warning_parameters_norm)
+void AdaptiveMomentEstimation::set_warning_parameters_norm(const type& new_warning_parameters_norm)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_warning_parameters_norm < 0.0)
@@ -349,10 +347,9 @@ void AdaptiveMomentEstimation::set_warning_parameters_norm(const double& new_war
 /// a warning message is written to the screen.
 /// @param new_warning_gradient_norm Warning norm of gradient vector value.
 
-void AdaptiveMomentEstimation::set_warning_gradient_norm(const double& new_warning_gradient_norm)
+void AdaptiveMomentEstimation::set_warning_gradient_norm(const type& new_warning_gradient_norm)
 {
    
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_warning_gradient_norm < 0.0)
@@ -378,7 +375,7 @@ void AdaptiveMomentEstimation::set_warning_gradient_norm(const double& new_warni
 /// screen and the program exits.
 /// @param new_error_parameters_norm Error norm of parameters vector value.
 
-void AdaptiveMomentEstimation::set_error_parameters_norm(const double& new_error_parameters_norm)
+void AdaptiveMomentEstimation::set_error_parameters_norm(const type& new_error_parameters_norm)
 {
    
 
@@ -407,10 +404,8 @@ void AdaptiveMomentEstimation::set_error_parameters_norm(const double& new_error
 /// and the program exits.
 /// @param new_error_gradient_norm Error norm of gradient vector value.
 
-void AdaptiveMomentEstimation::set_error_gradient_norm(const double& new_error_gradient_norm)
+void AdaptiveMomentEstimation::set_error_gradient_norm(const type& new_error_gradient_norm)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_error_gradient_norm < 0.0)
@@ -437,8 +432,6 @@ void AdaptiveMomentEstimation::set_error_gradient_norm(const double& new_error_g
 
 void AdaptiveMomentEstimation:: set_maximum_epochs_number(const int& new_maximum_epochs_number)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_maximum_epochs_number < 0.0)
@@ -463,10 +456,8 @@ void AdaptiveMomentEstimation:: set_maximum_epochs_number(const int& new_maximum
 /// Sets a new value for the minimum parameters increment norm stopping criterion.
 /// @param new_minimum_parameters_increment_norm Value of norm of parameters increment norm used to stop training.
 
-void AdaptiveMomentEstimation::set_minimum_parameters_increment_norm(const double& new_minimum_parameters_increment_norm)
+void AdaptiveMomentEstimation::set_minimum_parameters_increment_norm(const type& new_minimum_parameters_increment_norm)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_minimum_parameters_increment_norm < 0.0)
@@ -491,10 +482,8 @@ void AdaptiveMomentEstimation::set_minimum_parameters_increment_norm(const doubl
 /// Sets a new minimum loss improvement during training.
 /// @param new_minimum_loss_increase Minimum improvement in the loss between two iterations.
 
-void AdaptiveMomentEstimation::set_minimum_loss_increase(const double& new_minimum_loss_increase)
+void AdaptiveMomentEstimation::set_minimum_loss_increase(const type& new_minimum_loss_increase)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_minimum_loss_increase < 0.0)
@@ -520,7 +509,7 @@ void AdaptiveMomentEstimation::set_minimum_loss_increase(const double& new_minim
 /// This is used as a stopping criterion when training a neural network
 /// @param new_loss_goal Goal value for the loss.
 
-void AdaptiveMomentEstimation::set_loss_goal(const double& new_loss_goal)
+void AdaptiveMomentEstimation::set_loss_goal(const type& new_loss_goal)
 {
    loss_goal = new_loss_goal;
 }
@@ -530,10 +519,8 @@ void AdaptiveMomentEstimation::set_loss_goal(const double& new_loss_goal)
 /// This is used as a stopping criterion when training a neural network
 /// @param new_gradient_norm_goal Goal value for the norm of the error function gradient.
 
-void AdaptiveMomentEstimation::set_gradient_norm_goal(const double& new_gradient_norm_goal)
+void AdaptiveMomentEstimation::set_gradient_norm_goal(const type& new_gradient_norm_goal)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_gradient_norm_goal < 0.0)
@@ -567,10 +554,8 @@ void AdaptiveMomentEstimation::set_maximum_selection_error_increases(const int& 
 /// Sets a new maximum training time.
 /// @param new_maximum_time Maximum training time.
 
-void AdaptiveMomentEstimation::set_maximum_time(const double& new_maximum_time)
+void AdaptiveMomentEstimation::set_maximum_time(const type& new_maximum_time)
 {
-   
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_maximum_time < 0.0)
@@ -636,7 +621,6 @@ void AdaptiveMomentEstimation::set_reserve_selection_error_history(const bool& n
 void AdaptiveMomentEstimation::set_display_period(const int& new_display_period)
 {
    
-
    #ifdef __OPENNN_DEBUG__
 
    if(new_display_period <= 0)
@@ -698,7 +682,7 @@ OptimizationAlgorithm::Results AdaptiveMomentEstimation::perform_training()
    Tensor<type, 1> parameters = neural_network_pointer->get_parameters();
    Tensor<type, 1> parameters_increment(parameters_number);
 
-   double parameters_norm = 0.0;
+   type parameters_norm = 0.0;
 
    NeuralNetwork::ForwardPropagation forward_propagation(batch_instances_number, neural_network_pointer);
 
