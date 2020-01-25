f884d1731686757dd2f799660ac38b446d82d490
Guillermo Gestoso <guillermogestoso@artelnics.com>
2020-01-25 12:35:03 +0100

    statistics

--- blank/main.cpp
+++ blank/main.cpp
@@ -60,6 +60,11 @@ int main(void)
     {
         cout << "Hello Blank Application" << endl;
 
+        Tensor<type, 2> a(2,2);
+        a.setValues({{1,2},{3,4}});
+
+        cout << a(1,0) << endl;
+
         return 0;
 
     }
