a1d2c2b9614bb1b821744fcd16f48abda5109fb8
Roberto Lopez <robertolopez@artelnics.com>
2020-03-06 17:49:20 +0100

    Performance improvements

--- opennn/layer.h
+++ opennn/layer.h
@@ -294,6 +294,19 @@ protected:
     void logistic_derivatives(const Tensor<type, 2>&, Tensor<type, 3>&) const;
     void softmax_derivatives(const Tensor<type, 2>&, Tensor<type, 3>&) const;
 
+    void first_order_hard_sigmoid(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_hyperbolic_tangent(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_logistic(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_linear(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_threshold(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_symmetric_threshold(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_rectified_linear(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_scaled_exponential_linear(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_soft_plus(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_soft_sign(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+    void first_order_exponential_linear(const Tensor<type, 2>&, Tensor<type, 2>&, Tensor<type, 2>&) const;
+
+
     const Eigen::array<IndexPair<Index>, 1> A_BT = {IndexPair<Index>(1, 1)};
     const Eigen::array<IndexPair<Index>, 1> AT_B = {IndexPair<Index>(0, 0) };
     const Eigen::array<IndexPair<Index>, 1> A_B = {IndexPair<Index>(1, 0)};
