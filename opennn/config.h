1ee236757cbeb990c9eb83a846113500be02741f
Guillermo Gestoso <guillermogestoso@artelnics.com>
2020-03-13 13:41:59 +0100

    clean code

--- opennn/config.h
+++ opennn/config.h
@@ -4,10 +4,6 @@
 
 #include "../eigen/Eigen/src/Core/util/DisableStupidWarnings.h"
 
-//#pragma warning( push )
-//#pragma warning(disable:4267)
-//#pragma warning( pop )
-
 #define EIGEN_PERMANENTLY_DISABLE_STUPID_WARNINGS
 
 // For numeric limits
