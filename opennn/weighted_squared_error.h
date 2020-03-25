ef78bda126a86e48ccce8c700630f6da257bd841
Guillermo Gestoso <guillermogestoso@artelnics.com>
2020-03-25 12:30:13 +0100

    second order terms

--- opennn/weighted_squared_error.h
+++ opennn/weighted_squared_error.h
@@ -156,6 +156,114 @@ public:
 //        back_propagation.output_gradient = (outputs-targets)*((1.0-targets)*(static_cast<type>(-1.0))*negatives_weight + targets*positives_weight);
    }
 
+   void calculate_Jacobian_gradient(const DataSet::Batch& batch,
+                                       const NeuralNetwork::ForwardPropagation& forward_propagation,
+                                       LossIndex::SecondOrderLoss& second_order_loss) const
+      {
+       #ifdef __OPENNN_DEBUG__
+
+       check();
+
+       #endif
+
+       const Index trainable_layers_number = neural_network_pointer->get_trainable_layers_number();
+       const Index parameters_number = neural_network_pointer->get_parameters_number();
+
+       const Tensor<type, 2>& outputs = forward_propagation.layers(trainable_layers_number-1).activations_2d;
+       const Tensor<type, 2>& targets = batch.targets_2d;
+
+       Tensor<type, 2> errors(outputs.dimension(0), outputs.dimension(1));
+
+       const Index instances_number = data_set_pointer->get_training_instances_number();
+
+       Tensor<type, 2> expression(parameters_number, 1);
+
+       const type coefficient = (static_cast<type>(2.0)/static_cast<type>(instances_number));
+
+       switch(device_pointer->get_type())
+       {
+            case Device::EigenDefault:
+            {
+                DefaultDevice* default_device = device_pointer->get_eigen_default_device();
+
+                errors.device(*default_device) = (outputs - targets).square();
+
+                second_order_loss.gradient.device(*default_device) = second_order_loss.error_Jacobian.contract(errors, A_B).eval();
+
+                second_order_loss.gradient.device(*default_device) = second_order_loss.gradient*coefficient;
+
+                return;
+            }
+
+            case Device::EigenSimpleThreadPool:
+            {
+               ThreadPoolDevice* thread_pool_device = device_pointer->get_eigen_thread_pool_device();
+
+               errors.device(*thread_pool_device) = (outputs - targets).square();
+
+               expression.device(*thread_pool_device) = second_order_loss.error_Jacobian.contract(errors, AT_B).eval();
+
+               expression.device(*thread_pool_device) = coefficient*expression;
+
+               second_order_loss.gradient = expression.chip(0,1);
+
+               return;
+            }
+
+           case Device::EigenGpu:
+           {
+//                 GpuDevice* gpu_device = device_pointer->get_eigen_gpu_device();
+
+                return;
+           }
+       }
+  }
+
+   void calculate_hessian_approximation(LossIndex::SecondOrderLoss& second_order_loss) const
+   {
+        #ifdef __OPENNN_DEBUG__
+
+        check();
+
+        #endif
+
+        const Index instances_number = data_set_pointer->get_training_instances_number();
+
+        const type coefficient = (static_cast<type>(2.0)/static_cast<type>(instances_number));
+
+        switch(device_pointer->get_type())
+        {
+             case Device::EigenDefault:
+             {
+                 DefaultDevice* default_device = device_pointer->get_eigen_default_device();
+
+                 second_order_loss.hessian.device(*default_device) = second_order_loss.error_Jacobian.contract(second_order_loss.error_Jacobian, AT_B);
+
+                 second_order_loss.hessian.device(*default_device) = coefficient*second_order_loss.hessian;
+
+                 return;
+             }
+
+             case Device::EigenSimpleThreadPool:
+             {
+                ThreadPoolDevice* thread_pool_device = device_pointer->get_eigen_thread_pool_device();
+
+                second_order_loss.hessian.device(*thread_pool_device) = second_order_loss.error_Jacobian.contract(second_order_loss.error_Jacobian, AT_B);
+
+                second_order_loss.hessian.device(*thread_pool_device) = coefficient*second_order_loss.hessian;
+
+                return;
+             }
+
+            case Device::EigenGpu:
+            {
+//                 GpuDevice* gpu_device = device_pointer->get_eigen_gpu_device();
+
+                 return;
+            }
+        }
+   }
+
 
    // Error terms methods
 
