e32a0ab76e169e6012213b809fbda458e0f94178
Lenny Shleymovich <lenny@shleymovich.com>
2020-08-31 07:44:14 -0500

    No need to include eigen headers. opennn.h has everything needed here as well

--- examples/rosenbrock/main.cpp
+++ examples/rosenbrock/main.cpp
@@ -21,10 +21,6 @@
 
 #include "../../opennn/opennn.h"
 
-#include <../eigen/unsupported/Eigen/CXX11/Tensor>
-
-#include <../../eigen/unsupported/Eigen/CXX11/ThreadPool>
-
 using namespace OpenNN;
 using namespace std;
 using namespace Eigen;
