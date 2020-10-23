bc5ba83ec5d6cf68d3476778e34b4e0742a71bbd
jesusandres <jesusandres@artelnics.com>
2020-10-23 09:51:10 +0200

    Add E to number

--- opennn/opennn_strings.cpp
+++ opennn/opennn_strings.cpp
@@ -250,7 +250,7 @@ bool is_numeric_string(const string& str)
     }
     */
     return !str.empty() && std::find_if(str.begin(),
-        str.end(), [](unsigned char c) { return (!std::isdigit(c) && !std::isspace(c) && c != '-' && c != '+' && c != '.' && c != 'e'/* && c != 'E'*/); }) == str.end();
+        str.end(), [](unsigned char c) { return (!std::isdigit(c) && !std::isspace(c) && c != '-' && c != '+' && c != '.' && c != 'e' && c != 'E'/* && c != 'E'*/); }) == str.end();
 }
 
 
