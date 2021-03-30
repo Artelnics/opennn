b5a3ce45d7887f4bd8265ebad7509d9b772727ac
Roberto Lopez <robertolopez@artelnics.com>
2021-03-30 16:04:45 +0200

    Clean code

--- examples/airfoil_self_noise/main.cpp
+++ examples/airfoil_self_noise/main.cpp
@@ -38,8 +38,6 @@ int main()
         const Tensor<string, 1> inputs_names = data_set.get_input_variables_names();
         const Tensor<string, 1> targets_names = data_set.get_target_variables_names();
 
-        data_set.split_samples_random();
-
         const Index input_variables_number = data_set.get_input_variables_number();
         const Index target_variables_number = data_set.get_target_variables_number();
 
@@ -54,17 +52,12 @@ int main()
 
         // Neural network
 
-        const Index hidden_neurons_number = 10;
+        const Index hidden_neurons_number = 12;
 
-        Tensor<Index, 1> neural_network_architecture(3);
-        neural_network_architecture.setValues({input_variables_number, hidden_neurons_number, target_variables_number});
-
-        NeuralNetwork neural_network(NeuralNetwork::Approximation, neural_network_architecture);
+        NeuralNetwork neural_network(NeuralNetwork::Approximation, {input_variables_number, hidden_neurons_number, target_variables_number});
 
         neural_network.set_inputs_names(inputs_names);
         neural_network.set_outputs_names(targets_names);
-
-        neural_network.set_parameters_random();
 
         ScalingLayer* scaling_layer_pointer = neural_network.get_scaling_layer_pointer();
         scaling_layer_pointer->set_descriptives(inputs_descriptives);
@@ -78,7 +71,7 @@ int main()
 
         TrainingStrategy training_strategy(&neural_network, &data_set);
 
-        training_strategy.set_loss_method(TrainingStrategy::NORMALIZED_SQUARED_ERROR);
+        training_strategy.set_loss_method(TrainingStrategy::MEAN_SQUARED_ERROR);
 
         training_strategy.set_optimization_method(TrainingStrategy::GRADIENT_DESCENT);
 
@@ -93,21 +86,11 @@ int main()
 
         const TestingAnalysis::LinearRegressionAnalysis linear_regression_analysis = testing_analysis.perform_linear_regression_analysis()[0];
 
-        cout << "Intercept: " << linear_regression_analysis.intercept << endl;
-        cout << "Slope: " << linear_regression_analysis.slope << endl;
-        cout << "Correlation: " << linear_regression_analysis.correlation << endl;
+        linear_regression_analysis.print();
 
         // Save results
 
-        data_set.save("../data/data_set.xml");
-
         neural_network.save("../data/neural_network.xml");
-
-        training_strategy.save("../data/training_strategy.xml");
-
-        training_results.save("../data/training_results.dat");
-
-        linear_regression_analysis.save("../data/linear_regression_analysis.dat");
 
         cout << "End Airfoil Self-Noise Example" << endl;
 
