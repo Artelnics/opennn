dd31eef8809ef73a8c25861bc3abaf7fcf5539ca
Pablo <pablomartin@artelnics.com>
2021-04-27 14:16:48 +0200

    WSE

--- examples/rosenbrock/main.cpp
+++ examples/rosenbrock/main.cpp
@@ -44,7 +44,7 @@ int main()
 
         // Data Set
 
-        const Index samples = 1000000;
+        const Index samples = 10000;
         const Index variables = 10;
 
         DataSet data_set;
@@ -72,18 +72,18 @@ int main()
 
         TrainingStrategy training_strategy(&neural_network, &data_set);
 
-        training_strategy.set_loss_method(TrainingStrategy::MEAN_SQUARED_ERROR);
+        training_strategy.set_loss_method(TrainingStrategy::NORMALIZED_SQUARED_ERROR);
 
         training_strategy.get_loss_index_pointer()->set_regularization_method(LossIndex::NoRegularization);
 
 //        training_strategy.set_optimization_method(TrainingStrategy::ADAPTIVE_MOMENT_ESTIMATION);
-        training_strategy.set_optimization_method(TrainingStrategy::QUASI_NEWTON_METHOD);
+        training_strategy.set_optimization_method(TrainingStrategy::LEVENBERG_MARQUARDT_ALGORITHM);
 
 
 
         training_strategy.get_Levenberg_Marquardt_algorithm_pointer()->set_maximum_epochs_number(100000);
         training_strategy.get_Levenberg_Marquardt_algorithm_pointer()->set_maximum_selection_error_increases(1000000);
-        training_strategy.get_Levenberg_Marquardt_algorithm_pointer()->set_display_period(100);
+        training_strategy.get_Levenberg_Marquardt_algorithm_pointer()->set_display_period(10);
 //        training_strategy.get_Levenberg_Marquardt_algorithm_pointer()->set_minimum_damping_parameter(numeric_limits<type>::min());
 
 
