c25ed5c6db22b8f8096eb28d2e58cb77099906b4
Roberto Lopez <robertolopez@artelnics.com>
2021-07-19 12:14:01 +0200

    VS

--- examples/airfoil_self_noise/main.cpp
+++ examples/airfoil_self_noise/main.cpp
@@ -17,12 +17,12 @@
 
 // OpenNN includes
 
-#include "../opennn/opennn.h"
-
+#include "../../opennn/opennn.h"
+/*
 using namespace OpenNN;
 using namespace std;
 using namespace Eigen;
-
+*/
 int main()
 {
     try
@@ -30,7 +30,7 @@ int main()
         cout << "OpenNN. Blank application." << endl;
 
         srand(static_cast<unsigned>(time(nullptr)));
-
+/*
         DataSet data_set("C:/Users/Usuario/Documents/R_100000_samples_101_variables.csv", ',', true);
 
         const Index inputs_number = data_set.get_input_variables_number();
@@ -55,9 +55,10 @@ int main()
 
 
 
-
-
+*/
         cout << "Good bye!" << endl;
+
+        system("pause");
 
         return 0;
     }
