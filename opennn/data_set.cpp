7921f793d077fa9fd599f5fc30857e76360064f3
Pablo <pablomartin@artelnics.com>
2021-08-24 14:06:05 +0200

    time

--- opennn/data_set.cpp
+++ opennn/data_set.cpp
@@ -9964,10 +9964,12 @@ void DataSet::read_csv_1()
         if((is_date_time_string(data_file_preview(1)(i)) && data_file_preview(1)(i) != missing_values_label)
         || (is_date_time_string(data_file_preview(2)(i)) && data_file_preview(2)(i) != missing_values_label)
         || (is_date_time_string(data_file_preview(lines_number-2)(i)) && data_file_preview(lines_number-2)(i) != missing_values_label)
-        || (is_date_time_string(data_file_preview(lines_number-1)(i)) && data_file_preview(lines_number-1)(i) != missing_values_label)
+        || (is_date_time_string(data_file_preview(lines_number-1)(i)) && data_file_preview(lines_number-1)(i) != missing_values_label))
+/*
         || (data_file_preview(0)(i).find("time") != string::npos && is_numeric_string(data_file_preview(1)(i)) && is_numeric_string(data_file_preview(2)(i))
                                                                  && is_numeric_string(data_file_preview(lines_number-2)(i))
                                                                  && is_numeric_string(data_file_preview(lines_number-2)(i)) ))
+*/
         {
 
 
