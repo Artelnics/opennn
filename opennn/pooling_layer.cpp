447001bf517aebcfa60a176cc5ffbedc17b5c7bf
Roberto Lopez <robertolopez@artelnics.com>
2021-12-01 12:21:55 +0100

    Add long double to sums

--- opennn/pooling_layer.cpp
+++ opennn/pooling_layer.cpp
@@ -283,7 +283,7 @@ Tensor<type, 4> PoolingLayer::calculate_hidden_delta_convolutional(Convolutional
             const Index row_index = (tensor_index/output_columns_number)%output_rows_number;
             const Index column_index = tensor_index%output_columns_number;
 
-            type sum = type(0);
+            long double sum = 0.0;
 
             const Index lower_row_index = (row_index - next_layers_filter_rows)/next_layers_row_stride + 1;
             const Index upper_row_index = min(row_index/next_layers_row_stride + 1, next_layers_output_rows);
@@ -357,7 +357,7 @@ Tensor<type, 4> PoolingLayer::calculate_hidden_delta_pooling(PoolingLayer* next_
                     const Index row_index = (tensor_index/output_columns_number)%output_rows_number;
                     const Index column_index = tensor_index%output_columns_number;
 
-                    type sum = type(0);
+                    long double sum = 0.0;
 
                     const Index lower_row_index = (row_index - next_layers_pool_rows)/next_layers_row_stride + 1;
                     const Index upper_row_index = min(row_index/next_layers_row_stride + 1, next_layers_output_rows);
@@ -413,7 +413,7 @@ Tensor<type, 4> PoolingLayer::calculate_hidden_delta_pooling(PoolingLayer* next_
                     const Index row_index = (tensor_index/output_columns_number)%output_rows_number;
                     const Index column_index = tensor_index%output_columns_number;
 
-                    type sum = type(0);
+                    long double sum = 0.0;
 
                     const Index lower_row_index = (row_index - next_layers_pool_rows)/next_layers_row_stride + 1;
                     const Index upper_row_index = min(row_index/next_layers_row_stride + 1, next_layers_output_rows);
@@ -505,7 +505,7 @@ Tensor<type, 4> PoolingLayer::calculate_hidden_delta_perceptron(PerceptronLayer*
         //Index row_index = (tensor_index/output_columns_number)%output_rows_number;
         //Index column_index = tensor_index%output_columns_number;
 
-        //type sum = type(0);
+        //long double sum = 0.0;
 
         for(Index sum_index = 0; sum_index < next_layers_output_columns; sum_index++)
         {
@@ -556,7 +556,7 @@ Tensor<type, 4> PoolingLayer::calculate_hidden_delta_probabilistic(Probabilistic
         //const Index row_index = (tensor_index/output_columns_number)%output_rows_number;
         //const Index column_index = tensor_index%output_columns_number;
 
-        //type sum = type(0);
+        //long double sum = 0.0;
 
         for(Index sum_index = 0; sum_index < next_layers_output_columns; sum_index++)
         {
