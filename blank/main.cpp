19a7ea986dae720787b25371f2c91d5659a8792c
davidge807 <davidge807@usal.es>
2022-04-27 12:17:42 +0200

    missing main

---
+++ blank/main.cpp
@@ -0,0 +1,84 @@
+//   OpenNN: Open Neural Networks Library
+//   www.opennn.net
+//
+//   B L A N K   A P P L I C A T I O N
+//
+//   Artificial Intelligence Techniques SL
+//   artelnics@artelnics.com
+
+// System includes
+
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <time.h>
+
+// OpenNN includes
+
+#include "../opennn/opennn.h"
+#include "../opennn/text_analytics.h"
+
+using namespace opennn;
+using namespace std;
+using namespace Eigen;
+
+#include "data_set.h"
+
+int main()
+{
+    try
+    {
+        cout << "Blank script! " << endl;
+
+        TextAnalytics text_analytics;
+        Tensor<string,1> document(1);
+        Tensor<Tensor<string,1>,1> documents(1);
+
+        document(0) = " 1 issues cried holáóú rational 5 mindfulness imperational david@gmail.com";
+
+        documents(0) = document;
+
+        Tensor<Tensor<string,1>,1> tokens = text_analytics.tokenize(document);
+
+        string word = "designer";
+
+//        Tensor<Tensor<string,1>,1> documents_stemmed = text_analytics.apply_english_stemmer(documents);
+
+//        cout << documents_stemmed(0) << endl;
+
+        text_analytics.delete_numbers(documents);
+        text_analytics.delete_emails(documents);
+        text_analytics.replace_accented(documents);
+
+        cout << documents(0) << endl;
+
+        cout << "Goodbye!" << endl;
+
+        return 0;
+    }
+    catch(const exception& e)
+    {
+        cerr << e.what() << endl;
+
+        return 1;
+    }
+}
+
+
+// OpenNN: Open Neural Networks Library.
+// Copyright (C) Artificial Intelligence Techniques SL.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
