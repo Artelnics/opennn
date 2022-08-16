2ff9f94f43eb4a18c9a442ed3a892f5af05adbbe
davidge807 <davidgonzalez@artelnics.com>
2022-08-16 10:38:00 +0200

    cmake fixed

--- opennn/json_to_xml.h
+++ opennn/json_to_xml.h
@@ -9,29 +9,28 @@
 #ifndef JSON_TO_XML_H
 #define JSON_TO_XML_H
 
-#include <QXmlStreamWriter>
-#include <QJsonValue>
-#include <QJsonObject>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QObject>
-
-using namespace std;
+/// @todo
+//#include <QXmlStreamWriter>
+//#include <QJsonValue>
+//#include <QJsonObject>
+//#include <QJsonArray>
+//#include <QJsonDocument>
+//#include <QObject>
 
 namespace opennn
 {
 
-class JsonToXml: public QJsonValue
-{
-    static int guessPrecision(double&);
+//class JsonToXml: public QJsonValue
+//{
+//    static int guessPrecision(double&);
 
-    static void jsonValueToXml(const QJsonValue&, const QString&, QXmlStreamWriter&);
-    static void jsonObjectToXml(const QJsonObject&, const QString&, QXmlStreamWriter&);
-    static void jsonArrayToXml(const QJsonArray&, const QString&, QXmlStreamWriter&);
-public:
-    static QString jsonToXml(const QJsonDocument&, const QString& rootElementName = "root");
-    static QJsonDocument qstringToJson(const QString&);
-};
+//    static void jsonValueToXml(const QJsonValue&, const QString&, QXmlStreamWriter&);
+//    static void jsonObjectToXml(const QJsonObject&, const QString&, QXmlStreamWriter&);
+//    static void jsonArrayToXml(const QJsonArray&, const QString&, QXmlStreamWriter&);
+//public:
+//    static QString jsonToXml(const QJsonDocument&, const QString& rootElementName = "root");
+//    static QJsonDocument qstringToJson(const QString&);
+//};
 
 }
 #endif
