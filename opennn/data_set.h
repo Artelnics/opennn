4f5586e03c70de11ad0df9be2d01abbd25d975a1
davidge807 <davidgonzalez@artelnics.com>
2022-11-16 11:27:44 +0100

    text classifiation preview

--- opennn/data_set.h
+++ opennn/data_set.h
@@ -363,6 +363,7 @@ public:
     Tensor<type, 1> get_variable_data(const string&, const Tensor<Index, 1>&) const;
 
     Tensor<Tensor<string, 1>, 1> get_data_file_preview() const;
+    Tensor<string, 2> get_text_data_file_preview() const;
 
     Tensor<type, 2> get_subtensor_data(const Tensor<Index, 1>&, const Tensor<Index, 1>&) const;
 
