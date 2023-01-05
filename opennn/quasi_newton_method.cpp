b265d73e921bdfa1a27b36f4558e234d1f84d193
davidge807 <davidge807@gmail.com>
2023-01-05 11:26:29 +0100

    small fixes

--- opennn/quasi_newton_method.cpp
+++ opennn/quasi_newton_method.cpp
@@ -705,12 +705,6 @@ TrainingResults QuasiNewtonMethod::perform_training()
 
         // Update parameters
 
-        cout << "------------------------------" << endl;
-
-        optimization_data.print();
-
-        cout << "------------------------------" << endl;
-
         update_parameters(training_batch, training_forward_propagation, training_back_propagation, optimization_data);
 
         // Selection error
