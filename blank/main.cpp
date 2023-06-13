6816c783fcdb635f3fb476f637df8c038773fcd7
Alvaro7941 <alvaro.sanlab.1@gmail.com>
2023-06-13 10:49:20 +0200

    blank

--- blank/main.cpp
+++ blank/main.cpp
@@ -30,55 +30,78 @@ int main(int argc, char *argv[])
    try
    {
         cout << "OpenNN. Conv2D Example." << endl;
-        srand(static_cast<unsigned>(time(nullptr)));
 
         const Index batch_samples_number = 1;
 
-        const Index inputs_channels_number = 1;
-        const Index inputs_rows_number = 1;
-        const Index inputs_columns_number = 1;
+        const Index inputs_channels_number = 3;
+        const Index inputs_rows_number = 8;
+        const Index inputs_columns_number = 8;
 
-        const Index kernels_number = 1;
+        const Index targets_number = 1;
+
+        const Index kernels_number = 3;
         const Index kernels_channels_number = inputs_channels_number;
-        const Index kernels_rows_number = 1;
-        const Index kernels_columns_number = 1;
-
-        Tensor<Index, 1> input_variables_dimensions(3);
-        input_variables_dimensions(0) = inputs_channels_number;
-        input_variables_dimensions(1) = inputs_rows_number;
-        input_variables_dimensions(2) = inputs_columns_number;
-
-        Tensor<Index, 1> kernels_dimensions(4);
-        kernels_dimensions(0) = kernels_number;
-        kernels_dimensions(1) = kernels_channels_number;
-        kernels_dimensions(2) = kernels_rows_number;
-        kernels_dimensions(3) = kernels_columns_number;
-
-        ConvolutionalLayer convolutional_layer(input_variables_dimensions, kernels_dimensions);
-        convolutional_layer.set_activation_function(ConvolutionalLayer::ActivationFunction::Logistic);
-        convolutional_layer.set_biases_constant(1.0);
-        convolutional_layer.set_synaptic_weights_constant(1.0);
-
-        ConvolutionalLayerForwardPropagation convolutional_layer_forward_propagation(batch_samples_number,
-                                                                                     &convolutional_layer);
+        const Index kernels_rows_number = 3;
+        const Index kernels_columns_number = 3;
 
         Tensor<type, 4> x(batch_samples_number,
                           inputs_channels_number,
                           inputs_rows_number,
                           inputs_columns_number);
+        x.setConstant(1);
 
-        x.setConstant(type(1));
+        DataSet data_set(batch_samples_number,
+                         inputs_channels_number,
+                         inputs_rows_number,
+                         inputs_columns_number,
+                         targets_number);
 
-        bool switch_train = false;
+        data_set.set_training();
 
-        convolutional_layer.forward_propagate(x.data(),
-                                              input_variables_dimensions,
-                                              &convolutional_layer_forward_propagation,
-                                              switch_train);
+        Tensor<Index, 1> input_variables_dimensions(3);
+        input_variables_dimensions.setValues({inputs_channels_number,
+                                              inputs_rows_number,
+                                              inputs_columns_number});
 
-        cout << "dimensions:" << convolutional_layer_forward_propagation.outputs_dimensions << endl;
+        Tensor<Index, 1> kernels_dimensions(4);
+        kernels_dimensions.setValues({kernels_number,
+                                      kernels_channels_number,
+                                      kernels_rows_number,
+                                      kernels_columns_number});
 
-    convolutional_layer_forward_propagation.print();
+        NeuralNetwork neural_network;
+
+        ConvolutionalLayer convolutional_layer(input_variables_dimensions, kernels_dimensions);
+        neural_network.add_layer(&convolutional_layer);
+        convolutional_layer.set_activation_function(ConvolutionalLayer::ActivationFunction::Linear);
+
+        Tensor<Index, 1> convolutional_layer_outputs_dimensions = convolutional_layer.get_outputs_dimensions();
+
+        FlattenLayer flatten_layer(convolutional_layer_outputs_dimensions);
+        neural_network.add_layer(&flatten_layer);
+
+        Tensor<Index, 1> flatten_layer_outputs_dimensions = flatten_layer.get_outputs_dimensions();
+
+        PerceptronLayer perceptron_layer(flatten_layer_outputs_dimensions(0), 1);
+        perceptron_layer.set_activation_function(PerceptronLayer::ActivationFunction::Logistic);
+        neural_network.add_layer(&perceptron_layer);
+
+        neural_network.set_parameters_constant(1.0);
+
+
+        TrainingStrategy training_strategy(&neural_network, &data_set);
+
+        training_strategy.perform_training();
+
+//        Tensor<type, 2> inputs = data_set.get_input_data();
+//        Tensor<Index, 1> inputs_dimensions = get_dimensions(inputs);
+
+//        Tensor<type, 2> outputs(neural_network.get_outputs_number(), inputs_dimensions[0]);
+
+//        outputs = neural_network.calculate_outputs(inputs.data(), inputs_dimensions);
+
+//        cout << "outputs: " << outputs << endl;
+
         cout << "Bye!" << endl;
 
         return 0;
