f79aafec7635611a0f9dab0a613ecd0a884543ca
free43 <63957797+free43@users.noreply.github.com>
2023-12-19 22:36:08 +0100

    Rewritten mnist example to three diffrent models; one linear classifier and two convolution models. Also added the zipped mnist dataset. For letting the models train, the dataset has to be unzipped.

--- examples/mnist/main.cpp
+++ examples/mnist/main.cpp
@@ -38,7 +38,276 @@
 #include "../../opennn/opennn.h"
 #include "../../opennn/opennn_strings.h"
 
+#define NUMB_OF_TRAINING_DATA (60'000U)
+#define NUMB_OF_COLUMNS_PER_IMAGE (28U)
+#define NUMB_OF_ROWS_PER_IMAGE (28U)
+#define NUMB_OF_INPUT_VARIABLES (NUMB_OF_COLUMNS_PER_IMAGE*NUMB_OF_ROWS_PER_IMAGE)
+#define NUMB_OF_TESTING_DATA (10'000U)
+#define NUMB_OF_LABELS (10U)
+#define PATH_TO_DATA "data/"
+#define NAME_OF_TRAINING_FILE "train-images.idx3-ubyte"
+#define NAME_OF_TRAINING_LABEL_FILE "train-labels.idx1-ubyte"
+#define NAME_OF_TESTING_INPUT_FILE "t10k-images.idx3-ubyte"
+#define NAME_OF_TESTING_LABEL_FILE "t10k-labels.idx1-ubyte"
+
+
 using namespace opennn;
+
+Tensor<type, 2> get_data_from_file()
+{
+    Tensor<type, 2U> data(NUMB_OF_TRAINING_DATA + NUMB_OF_TESTING_DATA, NUMB_OF_INPUT_VARIABLES + NUMB_OF_LABELS);
+    ifstream training_data_input_file(PATH_TO_DATA NAME_OF_TRAINING_FILE, iostream::in | iostream::binary);
+    ifstream training_labeling_file(PATH_TO_DATA NAME_OF_TRAINING_LABEL_FILE, iostream::in | iostream::binary);
+    //ignore headers
+    training_data_input_file.ignore(16U);
+    training_labeling_file.ignore(8U);
+    auto fill = [&data](ifstream& input, ifstream& target, Index begin, Index end)
+    {
+        Tensor<type, 1U> labels(NUMB_OF_LABELS);
+        for(Index k = begin; k < end; ++k)
+        {
+            Index i = 0U;
+            char c{};
+            for(; i < NUMB_OF_INPUT_VARIABLES; ++i)
+            {
+                input.get(c);
+                //min max scaling
+                //data(k, i) = static_cast<type>(2) * (static_cast<type>(abs(c)) / type(255)) - static_cast<type>(1);
+                data(k, i) = static_cast<type>(abs(c)) / type(255);
+            }
+            target.get(c);
+            Index idx = static_cast<Index>(c);
+            labels.setZero();
+            labels(idx) = static_cast<type>(1);
+            for(Index j = 0U; j < NUMB_OF_LABELS; ++j, ++i)
+            {
+                data(k, i) = labels(j);
+            }
+        }
+
+    };
+    fill(training_data_input_file, training_labeling_file, 0U, NUMB_OF_TRAINING_DATA);
+    std::ifstream testing_data_input_file(PATH_TO_DATA NAME_OF_TESTING_INPUT_FILE, std::iostream::in | std::iostream::binary);
+    std::ifstream testing_data_label_file(PATH_TO_DATA NAME_OF_TESTING_LABEL_FILE, std::iostream::in | std::iostream::binary);
+    //ignore headers
+    testing_data_input_file.ignore(16U);
+    testing_data_label_file.ignore(8U);
+    fill(testing_data_input_file, testing_data_label_file, NUMB_OF_TRAINING_DATA, NUMB_OF_TRAINING_DATA + NUMB_OF_TESTING_DATA);
+    return data;
+}
+
+DataSet get_data_set()
+{
+    Tensor<type, 2> data = get_data_from_file();
+    DataSet data_set(data);
+    for(Index sample_indx = 0; sample_indx < static_cast<Index>(NUMB_OF_TRAINING_DATA * 0.8); sample_indx++)
+    {
+        data_set.set_sample_use(sample_indx, DataSet::SampleUse::Training);
+    }
+    for(Index sample_indx = static_cast<Index>(NUMB_OF_TRAINING_DATA * 0.8); sample_indx < NUMB_OF_TRAINING_DATA; sample_indx++)
+    {
+        data_set.set_sample_use(sample_indx, DataSet::SampleUse::Selection);
+    }
+    for(Index sample_indx = NUMB_OF_TRAINING_DATA; sample_indx < NUMB_OF_TRAINING_DATA + NUMB_OF_TESTING_DATA; sample_indx++)
+    {
+        data_set.set_sample_use(sample_indx, DataSet::SampleUse::Testing);
+    }
+    for(Index column_indx = 0; column_indx < NUMB_OF_INPUT_VARIABLES; column_indx++)
+    {
+        data_set.set_column_use(column_indx, DataSet::VariableUse::Input);
+        data_set.set_column_type(column_indx, DataSet::ColumnType::Numeric);
+    }
+    for(Index column_indx = NUMB_OF_INPUT_VARIABLES; column_indx < NUMB_OF_INPUT_VARIABLES + NUMB_OF_LABELS; column_indx++)
+    {
+        data_set.set_column_type(column_indx, DataSet::ColumnType::Binary);
+        data_set.set_column_use(column_indx, DataSet::VariableUse::Target);
+    }
+    data_set.set_columns_scalers(Scaler::NoScaling);
+    return data_set;
+}
+
+void test(const TestingAnalysis& ta)
+{
+    auto errors = ta.calculate_multiple_classification_testing_errors();
+    cout << "Sum squared error: " << errors(0) << endl;
+    cout << "Mean squared error: " << errors(1) << endl;
+    cout << "Root mean squared error: " << errors(2) << endl;
+    cout << "Normalized squared error: " << errors(3) << endl;
+    cout << "Cross-entropy erro: " << errors(4) << endl;
+
+    auto confusion_matrix = ta.calculate_confusion();
+    cout << "Confusion matrix:\n";
+    Index correct_predicted_values_number = 0;
+    for(Index i = 0; i < confusion_matrix.dimension(0); i++)
+    {
+        for(Index j = 0; j < confusion_matrix.dimension(1); j++)
+        {
+            if(i == j && i != confusion_matrix.dimension(0) - 1 && j != confusion_matrix.dimension(1) - 1)
+            {
+                correct_predicted_values_number += confusion_matrix(i, j);
+            }
+            cout << confusion_matrix(i, j) << "\t";
+        }
+        cout << endl;
+    }
+    const Index testing_samples_number = ta.get_data_set_pointer()->get_testing_samples_number();
+    const type accuracy = static_cast<type>(correct_predicted_values_number) / static_cast<type>(testing_samples_number);
+    
+    cout << "Accuracy: " << accuracy << endl;
+    cout << endl;
+}
+
+TrainingResults do_example1(NeuralNetwork& nn, TrainingStrategy& ts)
+{
+    auto ds = ts.get_data_set_pointer();
+    
+    Tensor<Index, 1> input_variable_dimension(1);
+    input_variable_dimension.setValues({NUMB_OF_INPUT_VARIABLES});
+    ds->set_input_variables_dimensions(input_variable_dimension);
+
+    PerceptronLayer* pcll = new PerceptronLayer(NUMB_OF_INPUT_VARIABLES, 128U, PerceptronLayer::ActivationFunction::RectifiedLinear);
+    ProbabilisticLayer* pl = new ProbabilisticLayer(pcll->get_neurons_number(), NUMB_OF_LABELS);
+    pl->set_activation_function(ProbabilisticLayer::ActivationFunction::Softmax);
+    nn.add_layer(pcll);
+    nn.add_layer(pl);
+    
+    return ts.perform_training();
+}
+
+TrainingResults do_example2(NeuralNetwork& nn, TrainingStrategy& ts)
+{
+    auto ds = ts.get_data_set_pointer();
+    Tensor<Index, 1> input_variable_dimension(3);
+    input_variable_dimension.setValues({NUMB_OF_ROWS_PER_IMAGE, NUMB_OF_COLUMNS_PER_IMAGE, 1});
+    ds->set_input_variables_dimensions(input_variable_dimension);
+
+    const Index batch_samples = 100U;
+    Tensor<Index, 1> conv_input_dimension(4);
+    conv_input_dimension.setValues({
+        NUMB_OF_ROWS_PER_IMAGE,
+        NUMB_OF_COLUMNS_PER_IMAGE,
+        1,
+        batch_samples
+    });
+    Tensor<Index, 1> conv_kernel_dimension(4);
+    conv_kernel_dimension.setValues({
+        5,
+        5,
+        1,
+        6
+    });
+
+    Tensor<Index, 1> pooling_dimension(2);
+    pooling_dimension.setValues({
+        2,
+        2,
+
+    });
+    ConvolutionalLayer* conv_layer = new ConvolutionalLayer(conv_input_dimension, conv_kernel_dimension);
+    conv_layer->set_convolution_type(ConvolutionalLayer::ConvolutionType::Same);
+    conv_layer->set_activation_function(ConvolutionalLayer::ActivationFunction::RectifiedLinear);
+
+    PoolingLayer* pooling_layer = new PoolingLayer(conv_layer->get_outputs_dimensions(), pooling_dimension);
+    pooling_layer->set_column_stride(2);
+    pooling_layer->set_row_stride(2);
+    pooling_layer->set_pooling_method(PoolingLayer::PoolingMethod::MaxPooling);
+
+    FlattenLayer* fl = new FlattenLayer(pooling_layer->get_outputs_dimensions());
+
+    PerceptronLayer* pcl = new PerceptronLayer(fl->get_outputs_number(), 32U, PerceptronLayer::ActivationFunction::RectifiedLinear);
+
+    ProbabilisticLayer* pll = new ProbabilisticLayer(pcl->get_neurons_number(), 10);
+    pll->set_activation_function(ProbabilisticLayer::ActivationFunction::Softmax);
+
+    nn.add_layer(conv_layer);
+    nn.add_layer(pooling_layer);
+    nn.add_layer(fl);
+    nn.add_layer(pcl);
+    nn.add_layer(pll);
+
+    return ts.perform_training();
+}
+
+TrainingResults do_example3(NeuralNetwork& nn, TrainingStrategy& ts)
+{
+    auto ds = ts.get_data_set_pointer();
+    Tensor<Index, 1> input_variable_dimension(3);
+    input_variable_dimension.setValues({NUMB_OF_ROWS_PER_IMAGE, NUMB_OF_COLUMNS_PER_IMAGE, 1});
+    ds->set_input_variables_dimensions(input_variable_dimension);
+
+    const Index batch_samples = 100U;
+    Tensor<Index, 1> conv_input_dimension(4);
+    conv_input_dimension.setValues({
+        NUMB_OF_ROWS_PER_IMAGE,
+        NUMB_OF_COLUMNS_PER_IMAGE,
+        1,
+        batch_samples
+    });
+    Tensor<Index, 1> conv_kernel_dimension0(4);
+    conv_kernel_dimension0.setValues({
+        5,
+        5,
+        1,
+        6
+    });
+    Tensor<Index, 1> conv_kernel_dimension1(4);
+    conv_kernel_dimension1.setValues({
+        5,
+        5,
+        6,
+        16
+    });
+    Tensor<Index, 1> pooling_dimension(2);
+    pooling_dimension.setValues({
+        2,
+        2,
+    });
+    ConvolutionalLayer* conv_layer0 = new ConvolutionalLayer(conv_input_dimension, conv_kernel_dimension0);
+    conv_layer0->set_convolution_type(ConvolutionalLayer::ConvolutionType::Same);
+    conv_layer0->set_activation_function(ConvolutionalLayer::ActivationFunction::Logistic);
+
+    PoolingLayer* pooling_layer0 = new PoolingLayer(conv_layer0->get_outputs_dimensions(), pooling_dimension);
+    pooling_layer0->set_column_stride(2);
+    pooling_layer0->set_row_stride(2);
+    pooling_layer0->set_pooling_method(PoolingLayer::PoolingMethod::AveragePooling);
+
+    ConvolutionalLayer* conv_layer1 = new ConvolutionalLayer(pooling_layer0->get_outputs_dimensions(), conv_kernel_dimension1);
+    conv_layer1->set_convolution_type(ConvolutionalLayer::ConvolutionType::Valid);
+    conv_layer1->set_activation_function(ConvolutionalLayer::ActivationFunction::Logistic);
+
+    PoolingLayer* pooling_layer1 = new PoolingLayer(conv_layer1->get_outputs_dimensions(), pooling_dimension);
+    pooling_layer1->set_column_stride(2);
+    pooling_layer1->set_row_stride(2);
+    pooling_layer1->set_pooling_method(PoolingLayer::PoolingMethod::AveragePooling);
+
+    FlattenLayer* fl = new FlattenLayer(pooling_layer1->get_outputs_dimensions());
+
+    PerceptronLayer* pcl0 = new PerceptronLayer(fl->get_outputs_number(), 120U, PerceptronLayer::ActivationFunction::Logistic);
+    PerceptronLayer* pcl1 = new PerceptronLayer(pcl0->get_neurons_number(), 84U, PerceptronLayer::ActivationFunction::Logistic);
+
+    ProbabilisticLayer* pll = new ProbabilisticLayer(pcl1->get_neurons_number(), 10);
+    pll->set_activation_function(ProbabilisticLayer::ActivationFunction::Softmax);
+
+    nn.add_layer(conv_layer0);
+    nn.add_layer(pooling_layer0);
+    nn.add_layer(conv_layer1);
+    nn.add_layer(pooling_layer1);
+    nn.add_layer(fl);
+    nn.add_layer(pcl0);
+    nn.add_layer(pcl1);
+    nn.add_layer(pll);
+
+    return ts.perform_training();
+}
+
+void save_errors(const TrainingResults& tr, string_view sv)        
+{
+    ofstream of(sv.data(), ios::out);
+    for(Index i = 0; i < tr.selection_error_history.size(); i++)
+    {
+        of << i + 1 << ' ' << tr.training_error_history(i) << ' ' << tr.selection_error_history(i) << '\n';
+    }
+}
 
 int main()
 {
@@ -47,118 +316,41 @@ int main()
         cout << "OpenNN. National Institute of Standards and Techonology (MNIST) Example." << endl;
 
         srand(static_cast<unsigned>(time(nullptr)));
+        DataSet ds = get_data_set();
+        
+        NeuralNetwork nn;
 
-        // Data set
+        TrainingStrategy ts(&nn, &ds);
+        ts.set_loss_method(TrainingStrategy::LossMethod::CROSS_ENTROPY_ERROR);
+        ts.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
+        ts.set_maximum_epochs_number(10);
+        ts.set_display(false);
 
-        DataSet data_set;
+        TestingAnalysis ta(&nn, &ds);
 
-        data_set.set_data_file_name("../data/images/");
+        cout << "Model 1: \n";
+        TrainingResults tr0 = do_example1(nn, ts);
+        tr0.print();
+        cout << "Training time: " << tr0.elapsed_time << " \n";
+        save_errors(tr0, "tr0");
+        test(ta);
+        nn.delete_layers();
 
-        data_set.read_bmp();
+        cout << "Model 2: \n";
+        TrainingResults tr1 = do_example2(nn, ts);
+        save_errors(tr1, "tr1");
+        tr1.print();
+        cout << "Training time: " << tr1.elapsed_time << " \n";
+        test(ta);
+        nn.delete_layers();
+        
 
-        data_set.scale_input_variables();
-
-        const Index input_variables_number = data_set.get_input_variables_number();
-        const Index target_variables_number = data_set.get_target_variables_number();
-
-        const Tensor<Index, 1> samples_indices = data_set.get_training_samples_indices();
-        const Index samples_number = samples_indices.size();
-
-        const Tensor<Index, 1> input_variables_indices = data_set.get_input_variables_indices();
-        const Tensor<Index, 1> target_variables_indices = data_set.get_target_variables_indices();
-
-        const Tensor<Index, 1> input_variables_dimensions = data_set.get_input_variables_dimensions();
-        const Index inputs_channels_number = input_variables_dimensions[0];
-        const Index inputs_rows_number = input_variables_dimensions[1];
-        const Index inputs_columns_number = input_variables_dimensions[2];
-
-        Tensor<Index, 1> convolutional_layer_inputs_dimensions(4);
-        convolutional_layer_inputs_dimensions[0] = inputs_rows_number;
-        convolutional_layer_inputs_dimensions[1] = inputs_columns_number;
-        convolutional_layer_inputs_dimensions[2] = inputs_channels_number;
-        convolutional_layer_inputs_dimensions[3] = samples_number;
-
-        const Index kernels_rows_number = 2;
-        const Index kernels_columns_number = 2;
-        const Index kernels_number = 1;
-        const Index kernels_channels_number = inputs_channels_number;
-
-        Tensor<Index, 1> convolutional_layer_kernels_dimensions(4);
-        convolutional_layer_kernels_dimensions(0) = kernels_rows_number;
-        convolutional_layer_kernels_dimensions(1) = kernels_columns_number;
-        convolutional_layer_kernels_dimensions(2) = kernels_number;
-        convolutional_layer_kernels_dimensions(3) = kernels_channels_number;
-
-        Tensor<Index, 1> flatten_layer_inputs_dimensions(4);
-        flatten_layer_inputs_dimensions(0) = inputs_rows_number-kernels_rows_number+1;
-        flatten_layer_inputs_dimensions(1) = inputs_columns_number-kernels_columns_number+1;
-        flatten_layer_inputs_dimensions(2) = kernels_number;
-        flatten_layer_inputs_dimensions(3) = samples_number;
-
-        // Neural network
-
-        NeuralNetwork neural_network;
-
-        ScalingLayer scaling_layer(input_variables_dimensions);
-        neural_network.add_layer(&scaling_layer);
-
-        ConvolutionalLayer* convolutional_layer = new ConvolutionalLayer(convolutional_layer_inputs_dimensions, convolutional_layer_kernels_dimensions);
-        neural_network.add_layer(convolutional_layer);
-
-        FlattenLayer flatten_layer(flatten_layer_inputs_dimensions);
-        neural_network.add_layer(&flatten_layer);
-
-        PerceptronLayer perceptron_layer(flatten_layer.get_outputs_dimensions()[0], target_variables_number);
-        neural_network.add_layer(&perceptron_layer);
-
-        // Training strategy
-
-        TrainingStrategy training_strategy(&neural_network, &data_set);
-
-        training_strategy.set_loss_method(TrainingStrategy::LossMethod::MEAN_SQUARED_ERROR);
-        training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
-        training_strategy.get_loss_index_pointer()->set_regularization_method(LossIndex::RegularizationMethod::L2);
-        training_strategy.get_adaptive_moment_estimation_pointer()->set_batch_samples_number(1000);
-        training_strategy.get_adaptive_moment_estimation_pointer()->set_maximum_epochs_number(10000);
-
-        training_strategy.perform_training();
-
-        // Testing analysis
-
-        Tensor<type, 4> inputs_4d;
-
-        const TestingAnalysis testing_analysis(&neural_network, &data_set);
-
-        Tensor<unsigned char,1> zero = data_set.read_bmp_image("../data/images/zero/0_1.bmp");
-        Tensor<unsigned char,1> one = data_set.read_bmp_image("../data/images/one/1_1.bmp");
-
-        vector<type> zero_int(zero.size()); ;
-        vector<type> one_int(one.size());
-
-        for(Index i = 0 ; i < zero.size() ; i++ )
-        {
-            zero_int[i]=(type)zero[i];
-            one_int[i]=(type)one[i];
-        }
-
-        Tensor<type, 2> inputs(2, zero.size());
-        Tensor<type, 2> outputs(2, neural_network.get_outputs_number());
-
-        Tensor<Index, 1> inputs_dimensions = get_dimensions(inputs);
-        Tensor<Index, 1> outputs_dimensions = get_dimensions(outputs);
-
-        const Tensor<Index, 2> confusion = testing_analysis.calculate_confusion();
-
-        outputs = neural_network.calculate_outputs(inputs.data(), inputs_dimensions);
-
-        cout << "\nInputs:\n" << inputs << endl;
-
-        cout << "\nOutputs:\n" << outputs << endl;
-
-        cout << "\nConfusion matrix:\n" << confusion << endl;
-
-
-        cout << "Bye!" << endl;
+        cout << "Model 3: \n";
+        TrainingResults tr2 = do_example3(nn, ts);
+        save_errors(tr2, "tr2");
+        tr2.print();
+        cout << "Training time: " << tr2.elapsed_time << " \n";
+        test(ta);
 
         return 0;
     }
