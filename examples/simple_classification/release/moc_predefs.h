f022243186ed1c353cb71b3ede1b921f2b036c2c
BrunoSanGom <sanchezgomez152002@gmail.com>
2024-01-24 13:19:35 +0100

    avx2

---
+++ examples/simple_classification/release/moc_predefs.h
@@ -0,0 +1,12 @@
+#define _MSC_EXTENSIONS 
+#define _MSC_VER 1938
+#define _MSC_FULL_VER 193833134
+#define _MSC_BUILD 0
+#define _M_AMD64 100
+#define _M_X64 100
+#define _WIN64 
+#define _WIN32 
+#define _CPPRTTI 
+#define _MT 
+#define _DLL 
+#define _UTF8 
