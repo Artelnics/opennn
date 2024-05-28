199936433d183df5ae086091031dcfed1f04e4e3
Pablo <pablomaju@gmail.com>
2024-05-29 01:05:11 +0200

    convolutional

--- opennn/convolutional_layer.cpp
+++ opennn/convolutional_layer.cpp
@@ -432,7 +432,7 @@ void ConvolutionalLayer::back_propagate(const Tensor<pair<type*, dimensions>, 1>
     const TensorMap<Tensor<type, 4>> deltas(deltas_pair(0).first,
                                             deltas_pair(0).second[0],
                                             deltas_pair(0).second[1],
-                                            inputs_pair(0).second[2],
+                                            deltass_pair(0).second[2],
                                             deltas_pair(0).second[3]);
 
     cout << "deltas: " << endl << deltas << endl;
@@ -542,7 +542,7 @@ void ConvolutionalLayer::back_propagate(const Tensor<pair<type*, dimensions>, 1>
 
     input_derivatives = error_combinations_derivatives.convolve(synaptic_weights,convolutions_dimensions);
 
-    cout << "Last inputs derivatives: " << endl << input_derivatives << endl;
+    cout << "Last inputs derivatives: " << endl << convolutional_layer_back_propagation->get_inputs_derivatives_pair()(0).first << endl;
 
     cout << "--------------------------------------------------------------------" << endl;
 }
