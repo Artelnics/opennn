e007eeb90e75bc0dbf1bfd891970d6053c62d404
RoberLopez <robertolopez@artelnics.com>
2024-09-23 19:23:15 +0200

    clean

--- opennn/adaptive_moment_estimation.cpp
+++ opennn/adaptive_moment_estimation.cpp
@@ -460,8 +460,7 @@ TrainingResults AdaptiveMomentEstimation::perform_training()
 
             results.resize_training_error_history(epoch+1);
 
-            if(has_selection) results.resize_selection_error_history(epoch+1);
-            else results.resize_selection_error_history(0);
+            results.resize_selection_error_history(has_selection ? epoch + 1 : 0);
 
             results.elapsed_time = write_time(elapsed_time);
 
