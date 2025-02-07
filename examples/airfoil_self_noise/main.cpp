3980d8803c1c717c571159510d47e6dd09b2d95f
David Rodrriguez <davidrodseg@gmail.com>
2025-02-07 15:50:14 +0100

    Fix of the scaling problem

--- examples/airfoil_self_noise/main.cpp
+++ examples/airfoil_self_noise/main.cpp
@@ -50,11 +50,11 @@ int main()
         //training_strategy.set_loss_method(TrainingStrategy::LossMethod::SUM_SQUARED_ERROR);
         //training_strategy.set_loss_method(TrainingStrategy::LossMethod::MINKOWSKI_ERROR); // @todo gives 0.56
 
-        // training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::QUASI_NEWTON_METHOD);
+        training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::QUASI_NEWTON_METHOD);
         // training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::CONJUGATE_GRADIENT);
         // training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::LEVENBERG_MARQUARDT_ALGORITHM); //Fail-Mean Squared error / Doesnt work with MINKOWSKI_ERROR / is not implemented yet with weighted squared error
         // training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::STOCHASTIC_GRADIENT_DESCENT);
-        training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
+        // training_strategy.set_optimization_method(TrainingStrategy::OptimizationMethod::ADAPTIVE_MOMENT_ESTIMATION);
 
         training_strategy.set_maximum_epochs_number(10000);
 
@@ -70,7 +70,7 @@ int main()
 
         // Testing analysis
 
-        neural_network.set_parameters_constant(0);
+        // neural_network.set_parameters_constant(0);
 
         TestingAnalysis testing_analysis(&neural_network, &data_set);
 
