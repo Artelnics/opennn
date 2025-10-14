b19381076d73b804d49971b6a12f0eadad49bfea
Danigp12 <danigp12@usal.es>
2025-10-14 11:38:23 +0200

    Dataset & CrossEntropy tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(filter) = "MinkowskiErrorTest*";
+    ::testing::GTEST_FLAG(filter) = "CrossEntropyError2d*";
 
     try {
         return RUN_ALL_TESTS();
