68d9547a92ab00bc98dc5129b2a231ec754f9b21
Lenny Shleymovich <lenny.shleymovich@blockfills.com>
2025-10-25 15:52:47 -0500

    catching up

--- opennn/genetic_algorithm.cpp
+++ opennn/genetic_algorithm.cpp
@@ -485,7 +485,7 @@ void GeneticAlgorithm::perform_selection()
 
 Index GeneticAlgorithm::get_selected_individuals_number() const
 {
-    return count(selection.data(), selection.data() + selection.size(), 1);
+    return count(selection.data(), selection.data() + selection.size(), true);
 }
 
 
