10d1c3fdae4292a312a72edfd45177dac4c2f268
David Prieto Santos <david.prisan18@gmail.com>
2026-05-15 11:28:48 +0200

    Standard text classification network fix

--- examples/emotion_analysis/main.cpp
+++ examples/emotion_analysis/main.cpp
@@ -58,7 +58,9 @@ int main()
         adam->set_learning_rate(float(0.0001));
         adam->set_display_period(20);
 
-        cout << "Training with GPU, it might take some time: " << endl;
+        cout << "Training with "
+             << (Configuration::instance().is_gpu() ? "GPU" : "CPU")
+             << ", it might take some time: " << endl;
         training_strategy.train();
 
         // Testing Analysis
